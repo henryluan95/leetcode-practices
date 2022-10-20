6b1c16fda72a58463d1f3561c7e8eb4f2d13ac13
Henry Luan <henryluan95@gmail.com>
2022-10-20 13:38:20 +0000

    Sync LeetCode submission - Single Number (javascript)

--- my-folder/problems/single_number/solution.js
+++ my-folder/problems/single_number/solution.js
@@ -2,25 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function (nums) {
-  //Create a total sum
-  let total = 0;
-  let uniqueTotal = 0;
-  //Create an array to track unique numbers
-  const uniqueNumbersArray = [];
-  //loop through the numbers array
-  for (let number of nums) {
+var singleNumber = function(nums) {
+    //Create a total sum
+    let total = 0
+    let uniqueTotal = 0
+    //Create an array to track unique numbers
+    const uniqueNumbersArray = []
+    //loop through the numbers array
+    for (let number of nums){
     //sum all the numbers
-    total += number;
-    //sum all the numbers that doesn't repeat
-    if (uniqueNumbersArray.indexOf(number) === -1) {
-      uniqueNumbersArray.push(number);
-      uniqueTotal += number;
+        total += number
+    //sum all the numbers that doesn't repeat 
+     if(uniqueNumbersArray.indexOf(number) === -1){
+         uniqueNumbersArray.push(number)
+         uniqueTotal+= number
+     }   
     }
-  }
-  //Find sum of repeat numbers
-  const sumOfRepeatedNumber = total - uniqueTotal;
+    //Find sum of repeat numbers
+    const sumOfRepeatedNumber = total - uniqueTotal
 
-  // return sum unique numbers minus sum all repeated numbers
-  return uniqueTotal - sumOfRepeatedNumber;
+    // return sum unique numbers minus sum all repeated numbers
+    return uniqueTotal - sumOfRepeatedNumber
 };
