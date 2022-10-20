2ccfb34fc80c2c0616fbcfe8b70549b93ddb266e
Henry Luan <henryluan95@gmail.com>
2022-10-20 13:47:43 +0000

    Sync LeetCode submission - Find the Difference (javascript)

---
+++ my-folder/problems/find_the_difference/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    //If s is empty, return t
+    if(s.length === 0){
+        return t
+    }
+    
+    //sort string s and t
+    const sortedS = s.split("").sort()
+    const sortedT = t.split("").sort()
+    //loop through string t
+    //if there is one character that doesn't match, return it
+   for( let i =0; i < sortedT.length; i++){
+       if(sortedT[i] !== sortedS[i]){
+           return sortedT[i]
+       }
+   }
+};
