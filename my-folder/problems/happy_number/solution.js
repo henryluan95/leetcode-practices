75bc3a8c9ce6fdfb17bad4f14daf09d85ff7de44
Henry Luan <henryluan95@gmail.com>
2022-10-23 17:27:19 +0000

    Sync LeetCode submission - Happy Number (javascript)

---
+++ my-folder/problems/happy_number/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(num) {
+    const sumTracker = [];
+    while (num !== 1) {
+    if(sumTracker.includes(num)) return false;
+    const tempNums = String(num)
+      .split("")
+      .map((n, index) => {
+        return +n * +n;
+      });
+    sumTracker.push(num);
+    num = tempNums.reduce((acc, cur) => acc + cur);
+  }
+  return true;
+};
