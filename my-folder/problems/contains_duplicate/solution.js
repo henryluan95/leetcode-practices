5c994b0af68d9303c74ab2828e77fad90e2da84c
Henry Luan <henryluan95@gmail.com>
2022-10-23 17:41:31 +0000

    Sync LeetCode submission - Contains Duplicate (javascript)

---
+++ my-folder/problems/contains_duplicate/solution.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const newSet = [...new Set(nums)]
+    return newSet.length !== nums.length
+};
