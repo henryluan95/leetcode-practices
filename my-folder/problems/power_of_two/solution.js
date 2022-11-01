62d3243e079d283c7907c044a7ca6a56bc52f18b
Henry Luan <henryluan95@gmail.com>
2022-11-01 15:52:47 +0000

    Sync LeetCode submission - Power of Two (javascript)

---
+++ my-folder/problems/power_of_two/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    //Check case where n equal 1
+    if(n === 1){
+        return true
+    }
+    
+    //If n isn't an even number, return false
+    if(n % 2 !== 0){
+        return false
+    }
+    
+    
+
+    //abs value of number is higher than 1, keep divide it by 2
+    //return true if it's equal 1
+    while (n >= 1){
+        if(n === 1){
+            return true
+        }
+        n = n /2;
+    }
+    
+    return false
+};
