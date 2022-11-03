b87cf9c8cc66f1fe02e2ef6e0b7257e9467d5190
Henry Luan <henryluan95@gmail.com>
2022-11-03 16:15:00 +0000

    Sync LeetCode submission - Valid Anagram (javascript)

---
+++ my-folder/problems/valid_anagram/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false
+    const sCount = {}
+    const tCount = {}
+    
+    for (let i = 0; i < s.length; i++){
+        if(!sCount[s[i]]) sCount[s[i]] = 1
+        if(!tCount[t[i]]) tCount[t[i]] = 1
+        sCount[s[i]]++
+        tCount[t[i]]++
+    }
+    
+    for (const item in sCount){
+        if(sCount[item] !== tCount[item]){
+            return false
+        }
+    }
+    return true
+};
