6e1a4717e3d90bd6bcbeedcf3c41f5a34fbad829
Henry Luan <henryluan95@gmail.com>
2022-11-09 14:40:06 +0000

    Sync LeetCode submission - Missing Number (javascript)

---
+++ my-folder/problems/missing_number/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+     //find the highest number in nums
+    const highestNumber = Math.max(...nums)
+    //Check if the missing number is included in nums
+    //If not, return the length as highest + missing number
+    if(nums.length > highestNumber){
+        return nums.length
+    }
+    
+    //If it already included
+    //find the sum of 0 to the highest number in nums
+    let sumOfNoMissingNumber = 0
+    for (let i = 0; i <= highestNumber; i++){
+        sumOfNoMissingNumber += i
+    }
+    //find the sum of all numbers in sum
+    const sumOfMissingNumber = nums.reduce((acc,cur) => cur + acc, 0)
+    //return the difference
+    return sumOfNoMissingNumber - sumOfMissingNumber
+};
