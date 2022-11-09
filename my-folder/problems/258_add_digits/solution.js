13cf9f3e6a3036483793b825ce585f64a4cb8cc8
Henry Luan <henryluan95@gmail.com>
2022-11-08 20:15:03 -0500

    add digit

---
+++ my-folder/problems/258_add_digits/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function (num) {
+  //Create a while loop with the condition of num.length is higher than 1
+  while (String(num).length > 1) {
+    //In the loop, String the number, split the number, Num the individual number and add it together
+    const numArray = String(num)
+      .split("")
+      .map((num) => Number(num));
+    num = numArray.reduce((acc, cur) => acc + cur, 0);
+  }
+  return num;
+};
