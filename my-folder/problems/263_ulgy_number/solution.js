30e5d450c50f855b551ea6f7a63c91d9de5ac1e9
Henry Luan <henryluan95@gmail.com>
2022-11-12 12:26:02 -0500

    ugly number

---
+++ my-folder/problems/263_ulgy_number/solution.js
@@ -0,0 +1,17 @@
+var isUgly = function (n) {
+  //check if n < 0, return false immediately
+  if (n < 1) return false;
+  //if not, continuously devide n by 2, 3 and 5
+  while (n % 2 === 0) {
+    n /= 2;
+  }
+  while (n % 3 === 0) {
+    n /= 3;
+  }
+  while (n % 5 === 0) {
+    n /= 5;
+  }
+  // return true if n equal 1 eventually
+  // return false if it isn't
+  return n === 1;
+};
