9d89526c78b43e717c55b80d1a779eb312cbdcd1
Henry Luan <henryluan95@gmail.com>
2022-11-16 12:51:20 -0500

    move zeroes

---
+++ my-folder/problems/283_move_zeros/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let pointer = 0;
+  //Loop through the array
+  for (let i = 0; i < nums.length; i++) {
+    //if the value of current index isn't 0, we swap, than increase pointer by 1
+    if (nums[i] !== 0) {
+      [nums[pointer], nums[i]] = [nums[i], nums[pointer]];
+      pointer++;
+    }
+  }
+};
