c55ff77fc0a6ffb71d14575a21d57ef04f6aef82
Henry Luan <henryluan95@gmail.com>
2022-11-22 10:07:25 -0500

    robot return to origin

---
+++ my-folder/problems/657_robot_origin/solution.js
@@ -0,0 +1,26 @@
+var judgeCircle = function (moves) {
+  let x = 0;
+  let y = 0;
+
+  //Using closure to create a function that change variables within a function
+  function moveRobot(moves) {
+    moves.split("").forEach((move) => {
+      switch (move) {
+        case "U":
+          x++;
+          break;
+        case "D":
+          x--;
+          break;
+        case "L":
+          y--;
+          break;
+        case "R":
+          y++;
+          break;
+      }
+    });
+  }
+  moveRobot(moves);
+  return x === 0 && y === 0;
+};
